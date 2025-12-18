bb0ff1cef8ffbf8fe7c7814453c5ce2a3ca40d03
Cyrus Kakkar <CYRUSKAKKAR@GMAIL.COM>
2025-12-18 11:46:58 +0530

    V 2.2

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Ensure three.js (an ESM-only package) is properly transpiled by Next.js
+  transpilePackages: ["three"],
 }
 
 module.exports = nextConfig
-
